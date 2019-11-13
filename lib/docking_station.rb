af3df4eb98ef5a64377576b315228f0e5b1c4013
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-11-13 17:22:21 +0000

    Created dock method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,10 @@
 class DockingStation
 
   def release_bike
+     Bike.new
+  end
+
+  def dock
   end
 end
 
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,4 +3,8 @@ describe DockingStation do
   it "release bike" do
     expect(subject).to respond_to(:release_bike)
   end
+
+  it "docks the bike at docking station" do
+    expect(subject).to respond_to(:dock)
+  end
 end
